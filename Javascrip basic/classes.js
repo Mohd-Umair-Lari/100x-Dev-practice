37f0916d08c8ae03d4cdfa9861b01ed0565ff242
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-07-22 23:40:19 +0530

    tinkered more with classes

--- Javascrip basic/classes.js
+++ Javascrip basic/classes.js
@@ -19,3 +19,11 @@ class Rectangle {
 const rect = new Rectangle(2, 4)
 const area = rect.area();
 console.log(area)
+
+const now = new Date(); // Current date and time
+console.log(now.toISOString()); // Outputs the date in ISO format
+
+const map = new Map();
+map.set('name', 'Alice');
+map.set('age', 30);
+console.log(map.get('name'));
