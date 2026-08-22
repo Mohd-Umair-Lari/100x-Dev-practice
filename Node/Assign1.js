c0f0097048ea864e0185d42c787c2cec30f4e22d
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-08-22 12:50:18 +0530

    tried to do assign ment 1

---
+++ Node/Assign1.js
@@ -0,0 +1,24 @@
+const fs=require('fs');
+const {Command}=require('commander');
+const program=new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count')
+  .description('CLI to count the number of lines')
+  .argument('<file>','file to count')
+  .action((file)=>{
+    fs.readFile(file,'utf-8',(err,data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            const lines=data.split(`\n`).length;
+            console.log('there are ${lines} lines in ${file}');
+        }
+    });
+  });
+
+program.parse();
