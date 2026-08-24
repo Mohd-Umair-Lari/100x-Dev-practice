5a420abdfcdb7f7a03224fa9ff3a3b1ed65f34bd
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-08-25 00:03:05 +0530

    Tried assignment 3

---
+++ Assignments/week-3/easy/bg-color-changer/script.js
@@ -0,0 +1,21 @@
+function Red(){
+    document.body.style.backgroundColor = "Red";
+}
+function Yellow(){
+    document.body.style.backgroundColor = "Yellow";
+}
+function Black(){
+    document.body.style.backgroundColor = "Black";
+}
+function Purple(){
+    document.body.style.backgroundColor = "Purple";
+}
+function Blue(){
+    document.body.style.backgroundColor = "Blue";
+}
+function Green(){
+    document.body.style.backgroundColor = "Green";
+}
+function Deafault(){
+    document.body.style.backgroundColor = "White";
+}
