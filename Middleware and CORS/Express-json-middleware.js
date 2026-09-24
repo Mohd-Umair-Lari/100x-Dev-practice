52210308009f6e7d91018343b89d4322da91e1c0
Mohd Umair Lari <umairlari0786@gmail.com>
2026-09-25 00:00:36 +0530

    Wrapped middleware for today

---
+++ Middleware and CORS/Express-json-middleware.js
@@ -0,0 +1,16 @@
+const express=require("express");
+
+const app=express();
+
+app.use(express.json());
+
+app.post("/sum",(req,res,next)=>{
+    const a=parseInt(req.body.a);
+    const b=parseInt(req.body.b);
+
+    res.json({
+        sum:a+b
+    });
+});
+
+app.listen(3000);
